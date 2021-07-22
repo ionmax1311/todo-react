1b5f03be194b2ccb8158c1d31d0ef3e59186cbe5
Max Ion <ionmax1311@gmail.com>
2021-07-22 12:01:30 +0300

    22.07

--- src/App.js
+++ src/App.js
@@ -14,169 +14,167 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import Badge from "@material-ui/core/Badge";
+
+import { v4 as uuid_v4 } from "uuid";
+
 import store from "./store";
 import { observer } from "mobx-react";
 
-const defaultState = [
-	{ id: 0, title: "React", done: false },
-	{ id: 1, title: "Vue", done: true },
-	{ id: 2, title: "Angular", done: false },
-];
+// const defaultState = [
+//   { id: 0, title: "React", done: false },
+//   { id: 1, title: "Vue", done: true },
+//   { id: 2, title: "Angular", done: false },
+// ];
 
 const useStyles = makeStyles((theme) =>
-	createStyles({
-		root: {
-			"& > *": {
-				margin: theme.spacing(1),
-			},
-		},
-		paper: {
-			// paddingBottom: 50,
-			// paddingTop: 15,
-			height: "auto",
-		},
-		header: {
-			height: "70px",
-			display: "flex",
-			alignItems: "center",
-			paddingLeft: "16px",
-			background: "#90a4ae",
-		},
-		badge: {
-			marginLeft: "20px",
-		},
-	}),
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+    paper: {
+      // paddingBottom: 50,
+      // paddingTop: 15,
+      height: "auto",
+    },
+    header: {
+      height: "70px",
+      display: "flex",
+      alignItems: "center",
+      paddingLeft: "16px",
+      background: "#90a4ae",
+    },
+    badge: {
+      marginLeft: "20px",
+    },
+  })
 );
 
 function App() {
-	const classes = useStyles();
-	const [tasks, setTasks] = useState(defaultState);
-	const activeTasks = tasks.filter((task) => !task.done);
-	const doneTasks = tasks.filter((task) => task.done);
-	const [openModal, setOpenModal] = React.useState(false);
-	let [editCurTask, setEditCurTask] = useState("");
-	let [curId, setCurId] = useState();
+  const classes = useStyles();
+  const [tasks, setTasks] = useState(store.defaultState);
+  const activeTasks = tasks.filter((task) => !task.done);
+  const doneTasks = tasks.filter((task) => task.done);
+  const [openModal, setOpenModal] = React.useState(false);
+  let [editCurTask, setEditCurTask] = useState("");
+  let [curId, setCurId] = useState();
 
-	const doneTask = (id) => {
-		setTasks(
-			tasks.map((task) =>
-				task.id === id
-					? { id, title: task.title, done: !task.done }
-					: task,
-			),
-		);
-	};
+  const doneTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { id, title: task.title, done: !task.done } : task
+      )
+    );
+  };
 
-	const deleteTask = (id) => {
-		setTasks(tasks.filter((task) => task.id !== id));
-	};
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-	const editTask = (title, id) => {
-		setOpenModal(true);
-		setEditCurTask(title);
-		console.log(id);
-		console.log(title);
-		setCurId(id);
-		// setTasks(
-		// 	tasks.map((task) =>
-		// 		task.id === id ? console.log(task.title) : null,
-		// 	),
-		// );
-	};
+  const editTask = (title, id) => {
+    setOpenModal(true);
+    setEditCurTask(title);
 
-	const handleCloseModal = () => {
-		setOpenModal(false);
-	};
+    setCurId(id);
+  };
 
-	const handleChangeEditTask = (e) => {
-		setEditCurTask(e.target.value);
-	};
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
-	const sendEditTask = (id) => {
-		// setEditCurTask(editCurTask);
-		console.log("editCurTask", editCurTask);
-		console.log("id-123", curId);
+  const handleChangeEditTask = (e) => {
+    setEditCurTask(e.target.value);
+  };
 
-		setTasks(
-			tasks.map((task) =>
-				task.id === curId
-					? { id, title: editCurTask, done: task.done }
-					: task,
-			),
-		);
-		console.log("tasks-new", tasks);
-		setOpenModal(false);
-	};
+  const sendEditTask = (id) => {
+    // setEditCurTask(editCurTask);
+    console.log("editCurTask", editCurTask);
+    console.log("id-123", curId);
 
-	const addTask = (task) => {
-		console.log("task", task);
-		console.log("tasks", tasks);
+    setTasks(
+      tasks.map((task) =>
+        task.id === curId ? { id, title: editCurTask, done: task.done } : task
+      )
+    );
+    console.log("tasks-new", tasks);
+    setOpenModal(false);
+  };
 
-		setTasks([
-			...tasks,
-			{
-				id: task.length,
-				title: task,
-				done: false,
-			},
-		]);
-	};
+  const addTask = (task, id) => {
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length,
+        title: task,
+        done: false,
+      },
+    ]);
+    console.log("addTask--task", task);
+    console.log("addTask--tasks", tasks);
+  };
 
-	return (
-		<Container maxWidth='sm'>
-			<Paper square className={classes.paper}>
-				<Paper className={classes.header}>
-					Active Tasks:
-					<Badge
-						badgeContent={activeTasks.length}
-						color='primary'
-						className={classes.badge}></Badge>
-				</Paper>
-				<div>
-					<Dialog
-						open={openModal}
-						onClose={handleCloseModal}
-						aria-labelledby='form-dialog-title'>
-						<DialogContent>
-							<TextField
-								autoFocus
-								margin='dense'
-								id='name'
-								label='edit message'
-								type='text'
-								fullWidth
-								defaultValue={editCurTask}
-								onChange={handleChangeEditTask}
-							/>
-						</DialogContent>
-						<DialogActions>
-							<Button onClick={handleCloseModal} color='primary'>
-								Cancel
-							</Button>
-							<Button
-								onClick={() => {
-									sendEditTask();
-								}}
-								color='primary'>
-								Send
-							</Button>
-						</DialogActions>
-					</Dialog>
-				</div>
-				<List className={classes.list}>
-					{tasks.map((task) => (
-						<Task
-							editTask={() => editTask(task.title, task.id)}
-							doneTask={() => doneTask(task.id)}
-							deleteTask={() => deleteTask(task.id)}
-							task={task}
-							key={task.id}></Task>
-					))}
-				</List>
-			</Paper>
-			<NewTask addTask={addTask}></NewTask>
-		</Container>
-	);
+  return (
+    <Container maxWidth="sm">
+      <Paper square className={classes.paper}>
+        <Paper className={classes.header}>
+          {console.log("store", store.defaultState)}
+          {console.log("uuid_v4()", uuid_v4())}
+          Active Tasks:
+          <Badge
+            badgeContent={activeTasks.length}
+            color="primary"
+            className={classes.badge}
+          ></Badge>
+        </Paper>
+        <div>
+          <Dialog
+            open={openModal}
+            onClose={handleCloseModal}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="edit message"
+                type="text"
+                fullWidth
+                defaultValue={editCurTask}
+                onChange={handleChangeEditTask}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseModal} color="primary">
+                Cancel
+              </Button>
+              <Button
+                onClick={() => {
+                  sendEditTask();
+                }}
+                color="primary"
+              >
+                Send
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+        <List className={classes.list}>
+          {tasks.map((task) => (
+            <Task
+              editTask={() => editTask(task.title, task.id)}
+              doneTask={() => doneTask(task.id)}
+              deleteTask={() => deleteTask(task.id)}
+              task={task}
+              key={uuid_v4()}
+            ></Task>
+          ))}
+        </List>
+      </Paper>
+      <NewTask addTask={addTask}></NewTask>
+    </Container>
+  );
 }
 
-export default App;
+export default observer(App);
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
-// import { observable, action, computed } from "mobx";
+import { observable, action, computed } from "mobx";
 
-// class Store {
-// 	@observable defaultState = [
-// 		{ id: 0, title: "React", done: false },
-// 		{ id: 1, title: "Vue", done: true },
-// 		{ id: 2, title: "Angular", done: false },
-// 	];
-// }
+class Store {
+  @observable defaultState = [
+    { id: 0, title: "React", done: false },
+    { id: 1, title: "Vue", done: true },
+    { id: 2, title: "Angular", done: false },
+  ];
+}
 
-// const store = new Store();
+const store = new Store();
 
-// export default store;
+export default store;
